348ea99c481cfae9ac0c0c8e2fcba759397529a0
Enrique De La Concepción <101868281+enriquedlc@users.noreply.github.com>
2023-09-01 22:27:37 +0100

    feature:sparkles: get all articles

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express"); // TODO: convert to ES modules
 
+const articles = require("./data/articles.json");
+
 const app = express();
 
 app.disable("x-powered-by");
 
 app.get("/", (_, res) => {
 	res.json({ message: "Hello World!" });
+});
+
+app.get("/articles", (_, res) => {
+	res.json(articles);
 });
 
 const PORT = process.env.PORT || 1234;
