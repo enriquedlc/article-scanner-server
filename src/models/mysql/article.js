cb906d2cba959555008da39e542a6b4f4a1c89d3
Enrique De La Concepción <101868281+enriquedlc@users.noreply.github.com>
2023-09-16 16:55:46 +0100

    :hammer: refactor get all function

--- src/models/mysql/article.js
+++ src/models/mysql/article.js
@@ -12,19 +12,16 @@ const connection = await mysql.createConnection(configuration);
 
 export class ArticleModel {
 	static async getAll({ createdAt }) {
-		if (createdAt) {
-			const [articles] = await connection.query(
-				"SELECT articleName, barcode, exhibition, shelf, warehouse, createdAt, updatedAt, BIN_TO_UUID(id) AS id FROM articles WHERE createdAt >= ?;",
-				[createdAt],
-			);
+		let query =
+			"SELECT articleName, barcode, exhibition, shelf, warehouse, createdAt, updatedAt, BIN_TO_UUID(id) AS id FROM articles";
 
+		if (createdAt) {
+			query += " WHERE createdAt >= ?";
+			const [articles] = await connection.query(query, [createdAt]);
 			return articles;
 		}
 
-		const [articles] = await connection.query(
-			"SELECT articleName, barcode, exhibition, shelf, warehouse, createdAt, updatedAt, BIN_TO_UUID(id) AS id FROM articles;",
-		);
-
+		const [articles] = await connection.query(query);
 		return articles;
 	}
 
