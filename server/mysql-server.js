bd2aec90fe1b1b25483817da0af4706ac2147d14
Enrique De La Concepción <101868281+enriquedlc@users.noreply.github.com>
2023-09-21 13:08:52 +0100

    :sparkles: mysql categories

--- server/mysql-server.js
+++ server/mysql-server.js
@@ -3,8 +3,10 @@ import { createServerApp } from "../app.js";
 // import the actual madel we want to use in the app
 import { ArticleModel } from "../src/models/mysql/article.js";
 import { UserModel } from "../src/models/mysql/user.js";
+import { CategoryModel } from "../src/models/mysql/category.js";
 
 createServerApp({
 	articleModel: ArticleModel,
 	userModel: UserModel,
+	categoryModel: CategoryModel,
 });
