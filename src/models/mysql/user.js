145d44a9626e2ee98d7e508bfd6353701cccd687
Enrique De La Concepción <101868281+enriquedlc@users.noreply.github.com>
2023-10-29 22:52:55 +0000

    :hammer:

--- src/models/mysql/user.js
+++ src/models/mysql/user.js
@@ -8,7 +8,7 @@ const configuration = {
 	user: process.env.USER,
 	password: process.env.PASSWORD,
 	database: process.env.DATABASE,
-	port: process.env.PORT,
+	port: process.env.DATABASEPORT,
 };
 
 const connection = await mysql.createConnection(configuration);
